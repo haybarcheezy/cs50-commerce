fee80813dbd176502a154256d62b5078845aba8e
Hayden Barnett <64932796+haybarcheezy@users.noreply.github.com>
2022-10-31 20:47:46 -0500

    initial

--- auctions/models.py
+++ auctions/models.py
@@ -2,5 +2,51 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# this is the model for users and it inherits AbstractUser
 class User(AbstractUser):
     pass
+
+# Listings
+
+
+class Listing(models.Model):
+    seller = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    category = models.CharField(max_length=64)
+    image_link = models.CharField(
+        max_length=200, default=None, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+# Bids
+class Bid(models.Model):
+    user = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    listingid = models.IntegerField()
+    bid = models.IntegerField()
+
+
+# Comments
+class Comment(models.Model):
+    user = models.CharField(max_length=64)
+    comment = models.CharField(max_length=64)
+    listingid = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# Watchlist
+
+
+class Watchlist(models.Model):
+    user = models.CharField(max_length=64)
+    listingid = models.IntegerField()
+
+
+# Winners
+class Winner(models.Model):
+    owner = models.CharField(max_length=64)
+    winner = models.CharField(max_length=64)
+    listingid = models.IntegerField()
+    winprice = models.IntegerField()
+    title = models.CharField(max_length=64, null=True)
